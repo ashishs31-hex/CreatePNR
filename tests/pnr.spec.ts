8583f2d7b023d9586e25f7ce9cf0b33026a74a82
ashishs31-hex <ashishs31@hexaware.com>
2026-04-09 11:19:28 -0400

    Update pnr.spec.ts

--- tests/pnr.spec.ts
+++ tests/pnr.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('open URL', async ({ page }) => {
+    
+    const name = process.env.NAME!;
+    const source = process.env.SOURCE!;
+    const destination = process.env.DESTINATION!;
+    const departureDate = process.env.DEPARTURE_DATE!;
+    const passengerCount = Number(process.env.PASSENGER_COUNT);
+
+    console.log({ name, source, destination, departureDate, passengerCount });
+
     await page.goto('${WORKSPACE}/test.htm');
     await page.fill('input[id="name"]', 'Ashish');
     await expect(page.locator('input[id="name"]')).toHaveValue('Ashish');
